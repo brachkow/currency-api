5e3b8b80141523d8d5bc9d415be997d24e35b726
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-04-25 20:27:57 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-```
-npm install
-npm run dev
-```
+# currency-api
+
+This is cloudflare worker based API that acts like proxy for openexchangerates api, that:
+
+1. caches `/history` response for queried date into Cloudflare KV (you can easily tweak caching frequency by editing date format — by default it is one response per day)
+2. allows to change base currency
+
+Example request:
 
 ```
-npm run deploy
+/?date=YYYY-MM-DD&base=UAH
 ```
+
+## Development
+
+1. Install dependencies with `pnpm i`
+2. Add your `OPENEXCHANGERATES_APP_ID` to `.dev.vars`
+3. Add `wrangler.toml` and fill it like shown in `wrangler.toml.example`
+
+Disable local mode if you want to put values into `_preview` KV
+
+## Production
+
+1. Run `pnpm wrangler secret put` to add `OPENEXCHANGERATES_APP_ID` with your openexchangerates App ID
+2. Add `wrangler.toml` and fill it like shown in `wrangler.toml.example`
+
+Then run `pnpm run deploy` to publish changes
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,54 @@
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import dayjs from 'dayjs';
+import { RatesResponse, Bindings, Currency } from './types';
+import currencyjs from 'currency.js';
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Bindings }>();
+app.use(logger());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
+app.get('/', async (c) => {
+  const { date, base } = c.req.query();
+  const OPENEXCHANGERATES_APP_ID = c.env.OPENEXCHANGERATES_APP_ID;
+
+  const key = dayjs(date ? date : dayjs()).format('YYYY-MM-DD');
+
+  let rates: RatesResponse['rates'] = {
+    ...(Object.fromEntries(
+      Object.values(Currency).map((currency) => [currency, 0]),
+    ) as Record<Currency, number>),
+  };
+
+  console.log('Trying to get rates from cache');
+  const cachedValue = await c.env.RATES.get(key);
+
+  if (cachedValue) {
+    console.log(`Cache hit for key ${key}`, cachedValue);
+    rates = JSON.parse(cachedValue) as RatesResponse['rates'];
+  } else {
+    console.log('Cache miss for key:', key);
+    rates = (
+      (await (
+        await fetch(
+          `https://openexchangerates.org/api/historical/${key}.json?app_id=${OPENEXCHANGERATES_APP_ID}`,
+        )
+      ).json()) as RatesResponse
+    ).rates;
+
+    await c.env.RATES.put(key, JSON.stringify(rates));
+  }
+
+  if (base) {
+    const baseRate = rates[base as Currency];
+    rates = Object.fromEntries(
+      Object.entries(rates).map(([currency, rate]) => [
+        currency,
+        currencyjs(rate, { precision: 10 }).divide(baseRate).value,
+      ]),
+    ) as RatesResponse['rates'];
+  }
+
+  return c.json(rates);
 });
 
 export default app;
