feb39adfb1b8c2cf49f6730d7c746054e711062e
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-04-26 23:28:24 +0200

    Add validator

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,68 @@
-import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import dayjs from 'dayjs';
 import { RatesResponse, Bindings, Currency } from './types';
 import currencyjs from 'currency.js';
+import { z } from 'zod';
+import { Hono } from 'hono';
+import { zValidator } from '@hono/zod-validator';
+
+const CurrencyValues = Object.values(Currency) as [string, ...string[]];
 
 const app = new Hono<{ Bindings: Bindings }>();
 app.use(logger());
 
-app.get('/', async (c) => {
-  const { date, base } = c.req.query();
-  const OPENEXCHANGERATES_APP_ID = c.env.OPENEXCHANGERATES_APP_ID;
+app.get(
+  '/',
+  zValidator(
+    'query',
+    z.object({
+      date: z.string().optional(),
+      base: z.enum(CurrencyValues).optional(),
+    }),
+  ),
+  async (c) => {
+    const { date, base } = c.req.valid('query');
+    const OPENEXCHANGERATES_APP_ID = c.env.OPENEXCHANGERATES_APP_ID;
 
-  const key = dayjs(date ? date : dayjs()).format('YYYY-MM-DD');
+    const key = dayjs(date ? date : dayjs()).format('YYYY-MM-DD');
 
-  let rates: RatesResponse['rates'] = {
-    ...(Object.fromEntries(
-      Object.values(Currency).map((currency) => [currency, 0]),
-    ) as Record<Currency, number>),
-  };
+    let rates: RatesResponse['rates'] = {
+      ...(Object.fromEntries(
+        Object.values(Currency).map((currency) => [currency, 0]),
+      ) as Record<Currency, number>),
+    };
 
-  console.log('Trying to get rates from cache');
-  const cachedValue = await c.env.RATES.get(key);
+    console.log('Trying to get rates from cache');
+    const cachedValue = await c.env.RATES.get(key);
 
-  if (cachedValue) {
-    console.log(`Cache hit for key ${key}`, cachedValue);
-    rates = JSON.parse(cachedValue) as RatesResponse['rates'];
-  } else {
-    console.log('Cache miss for key:', key);
-    rates = (
-      (await (
-        await fetch(
-          `https://openexchangerates.org/api/historical/${key}.json?app_id=${OPENEXCHANGERATES_APP_ID}`,
-        )
-      ).json()) as RatesResponse
-    ).rates;
+    if (cachedValue) {
+      console.log(`Cache hit for key ${key}`, cachedValue);
+      rates = JSON.parse(cachedValue) as RatesResponse['rates'];
+    } else {
+      console.log('Cache miss for key:', key);
+      rates = (
+        (await (
+          await fetch(
+            `https://openexchangerates.org/api/historical/${key}.json?app_id=${OPENEXCHANGERATES_APP_ID}`,
+          )
+        ).json()) as RatesResponse
+      ).rates;
 
-    await c.env.RATES.put(key, JSON.stringify(rates));
-  }
+      await c.env.RATES.put(key, JSON.stringify(rates));
+    }
 
-  if (base) {
-    const baseRate = rates[base as Currency];
-    rates = Object.fromEntries(
-      Object.entries(rates).map(([currency, rate]) => [
-        currency,
-        currencyjs(rate, { precision: 10 }).divide(baseRate).value,
-      ]),
-    ) as RatesResponse['rates'];
-  }
+    if (base) {
+      const baseRate = rates[base as Currency];
+      rates = Object.fromEntries(
+        Object.entries(rates).map(([currency, rate]) => [
+          currency,
+          currencyjs(rate, { precision: 10 }).divide(baseRate).value,
+        ]),
+      ) as RatesResponse['rates'];
+    }
 
-  return c.json(rates);
-});
+    return c.json(rates);
+  },
+);
 
 export default app;
